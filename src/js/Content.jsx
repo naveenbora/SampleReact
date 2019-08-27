931bcd87e7a701c602cf736a1534892f196ebb34
Bora Naveen <bnaveen@tavisca.in>
2019-08-27 11:58:16 +0530

    with states

--- src/js/Content.jsx
+++ src/js/Content.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-const person={
-    short:"",
-    firstname:"",
-    lastname:""
 
-}
 class Content extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            short:"",
+            firstname:"",
+            lastname:""
+        }
+    }
     render()
     {
-        //this.props.user[0].firstname="nav";
-       
+       // this.props.user[0].short="BNN";
+        
         return <div id="rerender">
          <div className="grid-item-middle" >
         <div className="nav-left">
                 <ul>
                  
                 {this.props.user.map(x1 => {
-                    
-                    
-                    return <a  className="side" onClick={() =>{this.update(x1)}}><li>{x1.short}</li></a>
+
+                    return <a  className="side" onClick={() =>{this.fill(x1)}}><li>{x1.short}</li></a>
                     
                 })}
                 </ul>
@@ -29,9 +31,9 @@ class Content extends React.Component{
                 
          <div className="heightdiv" >
                     <br></br>
-                <p><h2>Firstname</h2><input type="text" name="firstname"  value={person.firstname} className="text-box1"></input></p>
+                <p><h2>Firstname</h2><input type="text" name="firstname"  value={this.state.firstname} className="text-box1"></input></p>
                 
-                <p><h2>Lastname</h2><input type="text" name="Lastname" value={person.lastname} className="text-box1"></input></p>
+                <p><h2>Lastname</h2><input type="text" name="Lastname" value={this.state.lastname} className="text-box1"></input></p>
                 <p><button type="button" onClick={this.update.bind(this)}>submit </button></p>
                 
                 </div>
@@ -42,12 +44,21 @@ class Content extends React.Component{
         </div>
         </div>;
     }
-    update(x)
+    fill(x)
     {
-        person.short=x.short;
-        person.firstname=x.firstname;
-        person.lastname=x.lastname;
-        ReactDOM.render(<Content user={this.props.user}/>,document.getElementById('rerender'));
+        
+        this.setState({
+            
+            short:x.short,
+            firstname:x.firstname,
+            lastname:x.lastname
+        });
+        
+        
+    }
+    update()
+    {
+
     }
 }
 module.exports=Content;
